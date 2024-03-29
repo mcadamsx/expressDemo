3237eb7eda02af7dc0c3723ac6d212e964272c38
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-03-29 03:59:39 +0000

    get request

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 const expresss = require('express');
 const app = expresss();
 
+app.use(expresss.json())
+
+const courses = [
+    {id: 1, name : 'course 1'},
+    {id: 2, name : 'course 2'},
+    {id: 3, name : 'course 4'}
+]
+
+
 app.get('/',(req, res)=>{
     res.send("Hello World")
 })
-app.get('/api', (req,res)=>{
-    req.send("My time has come")
+app.get('/api/courses', (req,res)=>{
+    res.status(200).send(courses)
 })
 app.get('/api/courses/:id', (req, res)=>{
-    res.send(req.params.id)
+   const course =  courses.find(c => c.id === parseInt (req.params.id))
+   if(!course) res.status(404).send("The course with the given ID was not found")
+   res.status(200).send(course)
 })
+app.post('/api/courses', (req,res) => {
+    if(!req.body.name || req.body.name.length < 3){
+        res.status(400).send("Name is required ");
+        return;
+    }
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    }
+    courses.push(course);
+    res.send(course)
+}) 
 
 
 const port = process.env.PORT || 3000
