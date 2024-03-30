1e6d22b53ad8963313a4022b9f004e1aaf78b753
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-03-30 00:24:42 +0000

    get request

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const expresss = require('express');
-const app = expresss();
-
-app.use(expresss.json())
-
-const courses = [
-    {id: 1, name : 'course 1'},
-    {id: 2, name : 'course 2'},
-    {id: 3, name : 'course 4'}
-]
+const express = require('express')
+const Joi = require("joi")
+const app = express();
 
 
-app.get('/',(req, res)=>{
-    res.send("Hello World")
+
+app.get("/api", (req,res)=>{
+  res.send("Helo World")
 })
-app.get('/api/courses', (req,res)=>{
-    res.status(200).send(courses)
-})
-app.get('/api/courses/:id', (req, res)=>{
-   const course =  courses.find(c => c.id === parseInt (req.params.id))
-   if(!course) res.status(404).send("The course with the given ID was not found")
-   res.status(200).send(course)
-})
-app.post('/api/courses', (req,res) => {
-    if(!req.body.name || req.body.name.length < 3){
-        res.status(400).send("Name is required ");
-        return;
-    }
-    const course = {
-        id: courses.length + 1,
-        name: req.body.name
-    }
-    courses.push(course);
-    res.send(course)
-}) 
 
 
-const port = process.env.PORT || 3000
-app.listen(port, ()=>{console.log(`Listening on port ${port}...`);})
+app.post("/api", (req, res)=>{
+  const {error} = validateName(req.body);
+  if(error) return res.status(400).send(error.details[0].message)
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+function validateName(name){
+  const schema = {
+    name: Joi.string().min(3).required()
+  }
+
+  return Joi.valedate(name, schema)
+}
+
+
+
+const port = process.env.PORT || 4000
+app.listen(port, ()=>console.log(`Listening to port ${port}`))
