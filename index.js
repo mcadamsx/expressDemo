da29ca4c98747431285de37ad10a90c73648a762
Richard McAdams <93725119+mcadamsx@users.noreply.github.com>
2024-04-01 03:14:04 +0000

    connected to mongodb

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const users = require("./model/users.model");
 const app = express();
 
+require('dotenv').config()
+
 app.use(express.json());
 
 const PORT = process.env.PORT || 4000;
